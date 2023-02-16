bbc61a6fa98ef089543400861477d054cfd025cc
Anesu Chakaingesu <53548277+anesu4@users.noreply.github.com>
2023-02-16 15:09:50 +1100

    Initial Commit.
    Create a Contoso Pizza Shop with ASP.NET Core

---
+++ ContosoPizza/Services/PizzaService.cs
@@ -0,0 +1,47 @@
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Services;
+
+public static class PizzaService
+{
+    static List<Pizza> Pizzas { get; }
+    static int nextId = 3;
+    static PizzaService()
+    {
+        Pizzas = new List<Pizza>
+        {
+            new Pizza { Id=1, Name="Italian Sausage", IsGlutenFree = "false"},
+            new Pizza { Id=2, Name="Hawaiian", IsGlutenFree = "true" }, // Change state to false.
+            new Pizza { Id=3, Name="Lydia is Werid", IsGlutenFree="VERY TRUE"}
+            // pizza hawaiian special ISGluten == false
+        }
+    }
+    
+    public static List<Pizza> GetAll() => Pizzas;
+
+    public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
+
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        if(pizza is null)
+            return;
+
+        Pizzas.Remove(pizza);
+    }
+
+    public static void Update(Pizza pizza)
+    {
+        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+        if(index == -1)
+            return;
+
+        Pizzas[index] = pizza;
+    }
+}
